3dd78fa0cf6c7327cad7f71b924bb0d8cf5b016b
Budi <aku@budi.my.id>
2016-04-23 10:30:50 +0700

    #0402

--- config/database.php
+++ config/database.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', getenv('DB_CONNECTION')),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', getenv('DB_HOST')),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', getenv('DB_DATABASE')),
-            'username' => env('DB_USERNAME', getenv('DB_USERNAME')),
-            'password' => env('DB_PASSWORD', getenv('DB_PASSWORD')),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
