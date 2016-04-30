9c545ad31ea79398010c737e13514cab2a009efc
Budi <aku@budi.my.id>
2016-05-01 00:04:05 +0700

    #0402

--- app/Classes/Mail/MailClass.php
+++ app/Classes/Mail/MailClass.php
@@ -4,6 +4,17 @@ use Auth;
 use DB;
 
    class MailClass {
+	   public static function MailHeader($string)
+	   {
+			$header_arr = array();
+			$test = json_decode($string);
+			for($i=0;$i<count($test);$i++)
+			{
+				$header_arr[$test[$i][0]] = $test[$i][1];
+			}
+			return $header_arr;   
+	   }
+	   
 	   public static function setConf($name,$value,$idUser="")
 		{
 			if($idUser=="")
