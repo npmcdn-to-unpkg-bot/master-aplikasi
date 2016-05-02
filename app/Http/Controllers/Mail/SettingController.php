4e8f0d88d00103e3f727e61649068185cd5d2a02
Budi <aku@budi.my.id>
2016-05-02 18:20:15 +0700

    #0402

--- app/Http/Controllers/Mail/SettingController.php
+++ app/Http/Controllers/Mail/SettingController.php
@@ -32,6 +32,20 @@ class SettingController extends Controller
 				</div>');
 		}
 		
+		if($setting=="mail_setting")
+		{
+			$mail_name = $request->input('mail_name');
+			$mail_email = $request->input('mail_email');
+			
+			MailClass::setConf('mail_name',$mail_name,$user->id);
+			MailClass::setConf('mail_email',$mail_email,$user->id);
+			print('<div class="alert alert-success">
+					<ul>
+						Update Success          					
+					</ul>
+				</div>');
+		}
+		
 	}
 	
 	public function getSetting()
@@ -39,8 +53,9 @@ class SettingController extends Controller
 		$user = Auth::user();
 		$pushover_user = MailClass::getConf('pushover_user',$user->id);
 		$pushover_app = MailClass::getConf('pushover_app',$user->id);
-		
-		return view('mail/setting')->with('user',$user)->with('pushover_user',$pushover_user)->with('pushover_app',$pushover_app);
+		$mail_name = MailClass::getConf('mail_name',$user->id);
+		$mail_email = MailClass::getConf('mail_email',$user->id);
+		return view('mail/setting')->with('user',$user)->with('pushover_user',$pushover_user)->with('pushover_app',$pushover_app)->with('mail_name',$mail_name)->with('mail_email',$mail_email);
 	}
 }
 ?>
