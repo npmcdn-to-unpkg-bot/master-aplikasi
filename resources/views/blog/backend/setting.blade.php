30bf2844d1e7fdc99a244e8031bb503d9939c390
Budi <aku@budi.my.id>
2016-05-04 23:15:41 +0700

    #0402

--- resources/views/blog/backend/setting.blade.php
+++ resources/views/blog/backend/setting.blade.php
@@ -4,7 +4,13 @@
 @section('content')
 <link href="/bower_components/jquery-uploadfile/css/uploadfile.css" rel="stylesheet">
 <script src="/bower_components/jquery-uploadfile/js/jquery.uploadfile.min.js"></script>
+<div class="col-lg-12">
 <form method="post" action="/blog/setting">
+<div class="panel panel-primary">
+<div class="panel-heading">
+Title, Description, Header, and Socmed
+</div>
+<div class="panel-body">
 <input type="text" name="judul1" class="form-control" value="{{ $setting->judul1 }}" placeholder="Judul">
 <br>
 <input type="text" name="judul2" class="form-control" value="{{ $setting->judul2 }}" placeholder="Judul">
@@ -57,8 +63,13 @@ var uploadObj = $("#mulitplefileuploader").uploadFile(settings);
 <br>
 <input type="text" name="instagram" class="form-control" value="{{ $setting->instagram }}" placeholder="Instagram username">
 <br>
+</div>
+<div class="panel-footer">
 <input type="hidden" name="_token" id="_token" value="{{csrf_token()}}">
 <input type="hidden" name="key" value="{{$key}}">
 <input  class="btn btn-lg btn-success btn-block" type="submit" name="submit" value="Save">
+</div>
+</div>
 </form>
+</div>
 @endsection
